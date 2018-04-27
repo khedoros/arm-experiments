35d1ccb1aa8792bc2d7aff88f9e9fefc558d1449
khedoros <khedoros@gmail.com>
2018-04-27 12:42:11 -0700

    Working on disassembly output
    - Allows me to see output of decoding process and judge accuracy
    
    - Necessary for the way I usually do instruction trace output in my
      emulators

--- arm.h
+++ arm.h
@@ -35,7 +35,7 @@ char cond_str[][3] {
     "LT",
     "GT",
     "LE",
-    "AL",
+    "", //"AL", but always elided
     "NV"
 };
 
@@ -45,18 +45,6 @@ char alu_ops[][4] {
     "TST", "TEQ", "CMP", "CMN",
     "ORR", "MOV", "BIC", "MVN"};
 
-
-struct status {
-    unsigned mode:5;
-    unsigned thumb:1;
-    unsigned fiq_dis:1;
-    unsigned irq_dis:1;
-    unsigned reserved:20;
-    unsigned v:1;
-    unsigned c:1;
-    unsigned z:1;
-    unsigned n:1;
-};
 
 uint32_t instr_mask[] = { 0x0FFFFFF0,
                           0x0FBF0FFF,
@@ -129,146 +117,26 @@ char instr_names[][40] = { "BX                         ",
 
 union instr {
     struct {
-        union {
-            struct { // 0
-                unsigned operand2_0:12;
-                unsigned rd_0:4;
-                unsigned rn_0:4;
-                unsigned s_0:1;
-                unsigned opcode_0:4;
-            } __attribute__((packed));
-            struct { // 1
-                unsigned rm_1:4;
-                unsigned id3_1:4; //1001
-                unsigned rs_1:4;
-                unsigned rn_1:4;
-                unsigned rd_1:4;
-                unsigned s_1:1;
-                unsigned a_1:1;
-                unsigned id2_1:3; //000
-            } __attribute__((packed));
-            struct { // 2
-                unsigned rm_2:4;
-                unsigned id3_2:4; //1001
-                unsigned rn_2:4;
-                unsigned rd_lo_2:4;
-                unsigned rd_hi_2:4;
-                unsigned s_2:1;
-                unsigned a_2:1;
-                unsigned u_2:1;
-                unsigned id2_2:2; //01
-            } __attribute__((packed));
-            struct { // 3
-                unsigned rm_3:4;
-                unsigned id4_3:8; //00001001
-                unsigned rd_3:4;
-                unsigned rn_3:4;
-                unsigned id3_3:2; //00
-                unsigned b_3:1;
-                unsigned id2_3:2; //10
-            } __attribute__((packed));
-            struct { // 4
-                unsigned rn_4:4;
-                unsigned id2_4:21; //100101111111111110001
-            } __attribute__((packed));
-            struct { // 5
-                unsigned rm_5:4;
-                unsigned id4_5:1; //1
-                unsigned h_5:1;
-                unsigned s_5:1;
-                unsigned id3_5:5; //00001
-                unsigned rd_5:4;
-                unsigned rn_5:4;
-                unsigned l_5:1;
-                unsigned w_5:1;
-                unsigned id2_5:1; //0
-                unsigned u_5:1;
-                unsigned p_5:1;
-            } __attribute__((packed));
-            struct { // 6
-                unsigned offset2_6:4;
-                unsigned id4_6:1; //1
-                unsigned h_6:1;
-                unsigned s_6:1;
-                unsigned id3_6:1; //1
-                unsigned offset1_6:4;
-                unsigned rd_6:4;
-                unsigned rn_6:4;
-                unsigned l_6:1;
-                unsigned w_6:1;
-                unsigned id2_6:1; //1
-                unsigned u_6:1;
-                unsigned p_6:1;
-            } __attribute__((packed));
-            struct { // 7
-                unsigned offset_7:12;
-                unsigned rd_7:4;
-                unsigned rn_7:4;
-                unsigned l_7:1;
-                unsigned w_7:1;
-                unsigned b_7:1;
-                unsigned u_7:1;
-                unsigned p_7:1;
-            } __attribute__((packed));
-            struct { // 8
-                unsigned dont_care2_8:4;
-                unsigned id2_8:1; //1
-                unsigned dont_care1_8:20;
-            } __attribute__((packed));
-            struct { // 9
-                unsigned reg_list_9:16;
-                unsigned rn_9:4;
-                unsigned l_9:1;
-                unsigned w_9:1;
-                unsigned s_9:1;
-                unsigned u_9:1;
-                unsigned p_9:1;
-            } __attribute__((packed));
-            struct { // a
-                unsigned offset_a:24;
-                unsigned l_a:1;
-            } __attribute__((packed));
-            struct { // b
-                unsigned offset_b:8;
-                unsigned CPnum_b:4;
-                unsigned crd_b:4;
-                unsigned rn_b:4;
-                unsigned l_b:1;
-                unsigned w_b:1;
-                unsigned n_b:1;
-                unsigned u_b:1;
-                unsigned p_b:1;
-            } __attribute__((packed));
-            struct { // c
-                unsigned CRm_c:4;
-                unsigned id3_c:1; //0
-                unsigned CP_c:3;
-                unsigned CPnum_c:4;
-                unsigned crd_c:4;
-                unsigned crn_c:4;
-                unsigned l_c:1;
-                unsigned cp_op_c:3;
-                unsigned id2_c:1; //0
-            } __attribute__((packed));
-            struct { // d
-                unsigned CRm_d:4;
-                unsigned id3_d:1; //0
-                unsigned CP_d:3;
-                unsigned CPnum_d:4;
-                unsigned rd_d:4;
-                unsigned crn_d:4;
-                unsigned l_d:1;
-                unsigned cp_op_d:3;
-                unsigned id2_d:1; //0
-            } __attribute__((packed));
-            struct { // e
-                unsigned dont_care:24;
-                unsigned id2_e:1; //1
-            } __attribute__((packed));
-            unsigned blob:25;
-        } __attribute__((packed));
-        unsigned id:3;
+        unsigned reg3:4;
+        unsigned field7:4;
+        unsigned reg11:4;
+        unsigned reg15:4;
+        unsigned reg19:4;
+        unsigned op:8;
         unsigned cond:4;
-    } __attribute__((packed));
+    };
     uint32_t val:32;
-} __attribute__((packed));
+};
+
+struct status {
+    unsigned mode:5;
+    unsigned thumb:1;
+    unsigned fiq_dis:1;
+    unsigned irq_dis:1;
+    unsigned reserved:20;
+    unsigned v:1;
+    unsigned c:1;
+    unsigned z:1;
+    unsigned n:1;
+};
+
--- disassemble.cpp
+++ disassemble.cpp
@@ -3,18 +3,19 @@
 #include<cstdint>
 #include<vector>
 #include<cassert>
+#include<string>
 
 #include "arm.h"
 
 using namespace std;
 
-int decode_a(uint32_t inst);
-int decode_t(uint16_t inst);
+int decode_a(uint32_t inst, uint32_t offset);
+int decode_t(uint16_t inst, uint32_t offset);
+string instr_str(uint32_t op, uint32_t offset, int format);
 
 int main(int argc, char *argv[]) {
-    cout<<"instr: "<<sizeof(instr)<<" status: "<<sizeof(status)<<endl;
-    //assert(sizeof(instr) == 4);
-    //assert(sizeof(status) == 4);
+    assert(sizeof(instr) == 4);
+    assert(sizeof(status) == 4);
 
     if(argc != 2) {
         return 1;
@@ -39,7 +40,7 @@ int main(int argc, char *argv[]) {
         cout.width(8);
         cout.fill('0');
         cout<<(*(reinterpret_cast<uint32_t *>(&data[i])));
-        int result = decode_a(*(reinterpret_cast<uint32_t *>(&data[i])));
+        int result = decode_a(*(reinterpret_cast<uint32_t *>(&data[i])), i);
         if(result < 0) {
             cout<<"\tUNKNOWN"<<endl;
         }
@@ -47,14 +48,14 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-int decode_a(uint32_t inst) {
+int decode_a(uint32_t inst, uint32_t offset) {
     instr i;
     i.val = inst;
     cout<<"\t";
     int ret = -1;
     for(int j=0;j<22;j++) {
         if((i.val & instr_mask[j]) == instr_match[j]) {
-            cout<<dec<<j<<": "<<instr_names[j]<<endl;
+            cout<<dec<<j<<": "<<instr_str(inst, offset, j)<<endl;
             ret = j;
             break;
         }
@@ -65,4 +66,115 @@ int decode_a(uint32_t inst) {
 int decode_t(uint16_t inst) {
     int ret = 0;
     return ret;
+}
+
+uint32_t rotl(uint32_t val, uint32_t rot) {
+    rot &= 0x1f;
+    return (val<<rot) | (val>>((-rot)&0x1f));
+}
+
+uint32_t rotr(uint32_t val, uint32_t rot) {
+    rot &= 0x1f;
+    return (val>>rot) | (val<<((-rot)&0x1f));
+}
+
+string to_hex_string(uint32_t in) {
+    char digits[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+    string out("");
+    int first=0;
+    for(int i=7;i>=0;i--) {
+        int digit = ((in & (0xf<<(i*4)))>>(i*4));
+        if(digit && i > first) first = i;
+        if(first >= i) out += digits[digit];
+    }
+    return out;
+}
+
+int32_t sign_extend(uint32_t val, uint32_t bits) {
+    bits &= 0x1f;
+    if(!bits) return val;
+    if(val & (1<<(bits - 1))) { //high bit is set; return negative number
+        return (-int32_t(val))-1;
+    }
+    return val;
+}
+
+string instr_str(uint32_t op, uint32_t offset, int format) {
+    instr in;
+    in.val = op;
+    switch(format) {
+        case 0: //Branch and exchange
+            return string("BX") + cond_str[in.cond] + " R" + to_string(in.reg3);
+            break;
+        case 1: //MRS (copy status to register)
+            return string("MRS") + cond_str[in.cond] + " R" + to_string(in.reg15) + ", " + (((in.op & 4) == 4)?"SPSR":"CPSR");
+            break;
+        case 2: //MSR reg->reg (copy register to status)
+            return string("MSR") + cond_str[in.cond] + (((in.op & 4) == 4)?"SPSR":"CPSR") + ", R" + to_string(in.reg3);
+            break;
+        case 3: //MSR reg->reg_flg (copy register to flags)
+            return string("MSR") + cond_str[in.cond] + (((in.op & 4) == 4)?"SPSR":"CPSR") + "_flg, R" + to_string(in.reg3);
+            break;
+        case 4: //MSR immediate->reg_flg (copy shifted immediate to flags)
+            {
+                uint32_t immed = rotr(in.reg3 + (in.field7 * 16), in.reg11 * 2);
+                return string("MSR") + cond_str[in.cond] + (((in.op & 4) == 4)?"SPSR":"CPSR") + "_flg, R" + to_string(immed);
+            }
+            break;
+        case 5: //ALU ops (immediate-shifted reg operand)
+            {
+                string ret(string(alu_ops[(in.op & 0x1f)>>1]) + cond_str[in.cond] + (((in.op&1) == 1)?"S ":" "));
+                return ret;
+            }
+            break;
+        case 6: //ALU ops (reg-shifted reg operand)
+            {
+                string ret(string(alu_ops[(in.op & 0x1f)>>1]) + cond_str[in.cond] + (((in.op&1) == 1)?"S ":" "));
+                return ret;
+            }
+            break;
+        case 7: //ALU ops (immediate-shifted immediate operand)
+            {
+                string ret(string(alu_ops[(in.op & 0x1f)>>1]) + cond_str[in.cond] + (((in.op&1) == 1)?"S ":" "));
+                return ret;
+            }
+            break;
+        case 8: //Multiply
+            break;
+        case 9: //Multiply Long
+            break;
+        case 10: //Load/Store immediate
+            break;
+        case 11: //Load/Store immediate-shifted register
+            break;
+        case 12: //SWP operation
+            break;
+        case 13: //Load/Store half-word/signed-byte (register operand)
+            break;
+        case 14: //Load/Store half-word/signed-byte (immediate offset)
+            break;
+        case 15: //Load/Store multiple
+            break;
+        case 16: //Branch/Branch+Link
+            return string("B") + (((in.op & 16)==16)?"L":"") + cond_str[in.cond] + " 0x" + to_hex_string(sign_extend((in.val & 0x00ffffff)*4 + 8 + offset, 26));
+            break;
+        case 17: //Software Interrupt
+            return string("SWI 0x") + to_hex_string(in.val & 0x00ffffff);
+            break;
+        case 18: //CDP (coprocessor inst)
+            return string("CDP (unexpected instruction)");
+            break;
+        case 19: //LDC/STC (coprocessor load/store)
+            return string("LDC/STC (unexpected instruction)");
+            break;
+        case 20: //MRC/MCR (coprocessor register transfer?)
+            return string("MRC/MCR (unexpected instruction)");
+            break;
+        case 21: //Undefined
+            return string("<undefined>");
+            break;
+        default:
+            break;
+    }
+    return string("");
 }
